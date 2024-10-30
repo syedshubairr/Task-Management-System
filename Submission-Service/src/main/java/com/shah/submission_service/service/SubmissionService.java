422d49c80313aca4e8e316abe60e51e1a2e95d66
Syed Shubair Raza <110731556+syedshubairr@users.noreply.github.com>
2024-10-30 10:41:33 +0500

    Added microservices as submodules

---
+++ Submission-Service/src/main/java/com/shah/submission_service/service/SubmissionService.java
@@ -0,0 +1,17 @@
+package com.shah.submission_service.service;
+
+import com.shah.submission_service.model.Submission;
+
+import java.util.List;
+
+public interface SubmissionService {
+    public Submission submitTask(Long taskId, String githubLink, Long userId, String jwt) throws Exception;
+
+    public Submission getTaskSubmissionById(Long submissionId) throws Exception;
+
+    public List<Submission> getAllTaskSubmissions();
+
+    public List<Submission> getTaskSubmissionByTaskId(Long taskId);
+
+    public Submission acceptDeclineSubmission(Long id, String status) throws Exception;
+}
